c2beea5a06ee13b2e14d19ec9b1e1dcdbbb8d597
Vishal Pal <vishalqw78@gmail.com>
2022-09-13 00:51:20 +0530

    Added New Section and Styles

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ const skill = document.getElementById('skill');
 const project = document.getElementById('project');
 const education = document.getElementById('Education');
 const edu = document.getElementById('edu-board');
+const Cont = document.getElementById('Contact');
+const Cont_card = document.getElementById('card');
+const Cont_card2 = document.getElementById('card2');
+
+
+
 
 
 
@@ -86,6 +92,16 @@ toggle.addEventListener('click', () => {
         edu.classList.remove("bg-white");
         edu.classList.add('bg-gray-700');
 
+        Cont.classList.remove('bg-gray-100');
+        Cont.classList.add('bg-gray-900');
+        Cont.classList.remove('text-black');
+        Cont.classList.add('text-white');
+        Cont_card.classList.remove("bg-white");
+        Cont_card.classList.add('bg-gray-700');
+
+        Cont_card2.classList.remove("bg-white");
+        Cont_card2.classList.add('bg-gray-700');
+
         li.classList.remove('fa-moon');
         li.classList.add('fa-sun');
     } else {
@@ -111,6 +127,16 @@ toggle.addEventListener('click', () => {
 
         edu.classList.add("bg-white");
         edu.classList.remove('bg-gray-700');
+
+        Cont.classList.add('bg-gray-100');
+        Cont.classList.remove('bg-gray-900');
+        Cont.classList.add('text-black');
+        Cont.classList.remove('text-white');
+        Cont_card.classList.add("bg-white");
+        Cont_card.classList.remove('bg-gray-700');
+
+        Cont_card2.classList.add("bg-white");
+        Cont_card2.classList.remove('bg-gray-700');
 
         project.classList.remove('bg-gray-900');
         project.classList.add('bg-gray-100');
